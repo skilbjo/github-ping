733e41e3f08eb9cdc5b790a2f7c5f8f2daa73893
John Skilbeck <john.skilbeck@gmail.com>
2016-12-29 20:17:16 -0800

    '2016-12-29'

---
+++ README.md
@@ -0,0 +1,13 @@
+# ping
+
+## Last updated
+2016-12-29
+
+<<<<<<< HEAD
+Incremented: 38
+=======
+Incremented: 192
+>>>>>>> 75f78c925494371822899e9424571492e0a08f2f
+
+## What is this?? 
+Shell scripts to push commits to github errday. Throw it on a server and schedule the script with `cron`
---
+++ history.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+generate_dates() {
+  year=$1
+	start=$(gdate -d $year-01-01 +'%Y%m%d')
+	end=$(gdate -d $year-12-31 +'%Y%m%d')
+
+  local result=()	
+
+  while [ $start -le $end ]; do
+    result+=($(gdate -d $start +'%Y-%m-%d'))
+    start=$(gdate -d "$start +1 day" +'%Y%m%d')
+  done
+
+  push_github $result
+}
+
+push_github() {
+  result=$1
+
+  for d in ${result[@]}; do
+    sed -i '' 's/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}/'$d'/g' HISTORY.md
+    perl -pi -e 's/(Incremented: )([0-9]+)/"Incremented: ".($2+1)/e' HISTORY.md
+    git commit --date="$d" -am "'$d'" ; git push
+
+  done
+}
+
+generate_dates 2012
+
+exit 0
